3592cbb7f1e3806ed0d0bb59bd3b003781f30af1
Heather813903 <heathersmith9378@gmail.com>
2025-11-18 17:48:31 -0500

    typo

--- 01-node-tutorial/answers/10-fs-sync.js
+++ 01-node-tutorial/answers/10-fs-sync.js
@@ -9,6 +9,6 @@ writeFileSync("./temporary/fileA.txt", "This is line 1\n");
 writeFileSync("./temporary/fileA.txt", "This is line 2\n", { flag: "a" });
 writeFileSync("./temporary/fileA.txt", "This is line 3\n", { flag: "a" });
 
-const result = readFileSync("./temporary/fileA.text", "utf8");
+const result = readFileSync("./temporary/fileA.txt", "utf8");
 
 console.log(result);
