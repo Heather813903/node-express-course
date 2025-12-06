1ffc0289083a5145991536ce97da9d6424248aa7
Heather813903 <heathersmith9378@gmail.com>
2025-12-06 17:47:06 -0500

    Add product API routes and query filtering features

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,67 @@
-console.log('Express Tutorial')
+const express = require('express');
+const { products } = require("./data");
+
+const app = express();
+
+app.use(express.static("./public"));
+
+app.get("/api/v1/test", (req,res) => {
+    res.json({ message: "It worked!" });
+
+});
+
+app.get("/api/v1/products", (req,res) => {
+    res.json({ message: "Look at our products!" });
+
+});
+
+app.get("/api/v1/products/:productID", (req, res) => {
+    const idToFind = parseInt(req.params.productID, 10);
+
+    if (Number.isNaN(idToFind)) {
+        return res.status(404).json({ message: "That product was not found." });
+    }
+
+    const product = products.find((p) => p.id === idToFind);
+
+    if (!product) {
+        return res.status(404).json({ message: "That product was not found." });
+    }
+    res.json(product);
+});
+
+app.get("/api/v1/query", (req, res) => {
+    const { search, limit } = req.query;
+
+    let filteredProducts = [...products];
+
+    if (search) {
+        filteredProducts = filteredProducts.filter((p) => {
+            return p.name.startsWith(search);
+        });
+    }
+
+    if (req.query.maxPrice) {
+        const priceLimit = Number(req.query.maxPrice);
+        filteredProducts = filteredProducts.filter((p) => p.price < priceLimit);
+    }
+
+
+    if (limit) {
+        filteredProducts = filteredProducts.slice(0, Number(limit));
+    }
+
+
+    res.json(filteredProducts);
+});
+
+
+
+app.all("*", (req,res) => {
+    res.status(404).send("Page not found");
+});
+
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+
+});
