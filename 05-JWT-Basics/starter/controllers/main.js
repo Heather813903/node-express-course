a338d1c40ccb3c10ad1b48a5ff4ed8d9d0e5ff1f
Heather813903 <heathersmith9378@gmail.com>
2026-01-19 09:12:00 -0500

    Fix error handling and JWT error file names

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -5,15 +5,15 @@ const login = async (req, res) => {
   const { username, password } = req.body;
 
   if (!username || !password) {
-    throw new BadRequestError("Please provide email and password");
+    throw new BadRequestError("Please provide username and password");
   }
   const id = new Date().getDate();
 
   const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
-    expiresIn: "30d",
+    expiresIn: "24h",
   });
 
-  res.status(200).json({ msg: "user created", token });
+  res.status(200).json({ token });
 };
 
 const dashboard = async (req, res) => {
